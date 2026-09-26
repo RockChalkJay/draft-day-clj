15f4b756dbfd6751e1f1b70ce9cc3b603b759158
Jason H <rcj.shaky005@passmail.net>
2026-09-25 20:25:33 -0600

    nREPL support

--- src/clj/draft_day/server.clj
+++ src/clj/draft_day/server.clj
@@ -2,15 +2,20 @@
   "http-kit server entry point: serves the compiled SPA + the JSON API
   (draft-day.api.routes). Stateless — the browser owns and re-sends draft state."
   (:require [org.httpkit.server :as http]
+            [nrepl.server :as nrepl]
             [draft-day.api.routes :as routes])
   (:gen-class))
 
 (defonce ^:private server (atom nil))
+(defonce ^:private nrepl-server (atom nil))
 
 (defn stop! []
   (when-let [s @server]
     (s)
-    (reset! server nil)))
+    (reset! server nil))
+  (when-let [n @nrepl-server]
+    (n)
+    (reset! nrepl-server nil)))
 
 (defn start! [port]
   (stop!)
@@ -19,4 +24,9 @@
 (defn -main [& _]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
     (start! port)
-    (println (str "draft-day-clj server on http://localhost:" port))))
+    (println (str "draft-day-clj server on http://localhost:" port))
+    (let [s (nrepl/start-server :port 0
+                                :bind "127.0.0.1")]
+      (reset! nrepl-server s)
+      (spit ".nrepl-port" (:port s))
+      (println (str "nREPL on port " (:port s))))))
